64ba5c1b5d4ae5cd93f7ac2e34b33649598957e9
Duc Vu <de.yd315@gmail.com>
2019-06-16 20:25:29 -0500

    imported more libraries

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import fs from 'fs'
+import bodyParser from 'body-parser'
+import _ from 'lodash'
 
 const app = express()
 
@@ -7,4 +10,15 @@ app.get('/', (req, res) => {
         res.status(500).send(error)
     })
 })
-app.listen(3000)
+
+// app.get('/home', (req, res) => {
+//     fs.readFile('/Views/index.html', (err, buffer) => {
+//         const html = buffer.toString()
+//         res.setHeader('Content-Type', 'text/html')
+//         res.send(html)
+//     })
+// })
+
+app.listen(3000, () => {
+
+})
